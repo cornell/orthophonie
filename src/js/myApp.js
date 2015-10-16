96a808ea7f48e11af4693d76867b89cf69ead6dc
cornell <SMazurais@SYFADIS55.CONDATE.com>
2015-10-16 18:20:45 +0200

    Ajout menu responsive

--- src/js/myApp.js
+++ src/js/myApp.js
@@ -1,27 +1,42 @@
 (function(){
-	
+
+	// navigation	
 	if(document.querySelector('#js-accueil') != null){
 
 		var el = document.querySelector('.nav-accueil');
-		el.className += ' ' + 'nav-accueil--active';
+		el.classList.add('nav-accueil--active');
 	}
 	else if(document.querySelector('#js-prevention') != null){
 
 		el = document.querySelector('.nav-prevention');
-		el.className += ' ' + 'nav-prevention--active';
+		el.classList.add('nav-prevention--active');
 	}
 	else if(document.querySelector('#js-recherche') != null){
 
 		el = document.querySelector('.nav-recherche');
-		el.className += ' ' + 'nav-recherche--active';
+		el.classList.add('nav-recherche--active');
 	}
 	else if(document.querySelector('#js-formation') != null){
 
 		el = document.querySelector('.nav-formation');
-		el.className += ' ' + 'nav-formation--active';
+		el.classList.add('nav-formation--active');
 	} 
-	
+	else if(document.querySelector('#js-quiSommesNous') != null){
 
-
+		el = document.querySelector('.nav-quiSommesNous');
+		el.classList.add('nav-quiSommesNous--active');
+	}
 	
+	document.querySelector('.navIcon').addEventListener('click', function(e){
+		
+		el = document.querySelector('.nav');
+		el.classList.toggle('nav--hidden');
+		el.classList.toggle('nav--block');
+		
+		el = document.querySelector('.navIcon');
+		el.classList.toggle('is-opened');
+		
+		// el = document.querySelector('.navIcon-container');
+		// el.classList.toggle('navIcon-container--hide');
+	});
 })();
